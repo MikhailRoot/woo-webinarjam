36f81b0b11c4e8c2c1529f5839fb742201005a34
Mikhail Durnev <MikhailD.101@gmail.com>
2018-07-24 22:13:16 +0300

    fixed metabox error in case of order error

--- includes/order-webinarjam-metabox.php
+++ includes/order-webinarjam-metabox.php
@@ -24,15 +24,17 @@ function webinarjam_add_order_metaboxes()
 function webinarjam_order_metabox_content()
 {
     $reg_result=__webinarjam_get_webinar_registration_result_from_order();
-    echo '<style> .wbj_wrapper{position: relative;} .wbj_row{display: block;}  .wbj_row label{display: block; font-weight: bolder;} .wbj_row input{width: 100%;}  </style>';
-    echo '<div class="wbj_wrapper">';
-    foreach($reg_result as $name=>$value){
-     echo '
+    if(!is_null($reg_result) && (is_object($reg_result) || is_array($reg_result)) ) {
+        echo '<style> .wbj_wrapper{position: relative;} .wbj_row{display: block;}  .wbj_row label{display: block; font-weight: bolder;} .wbj_row input{width: 100%;}  </style>';
+        echo '<div class="wbj_wrapper">';
+        foreach ($reg_result as $name => $value) {
+            echo '
      <div class="wbj_row">
-        <label for="reg_result_'.$name.'">'.$name.'</label>
-        <input id="reg_result_'.$name.'" type="text"  value="'.$value.'">
+        <label for="reg_result_' . $name . '">' . $name . '</label>
+        <input id="reg_result_' . $name . '" type="text"  value="' . $value . '">
      </div>';
+        }
+        echo '</div>';
     }
-    echo '</div>';
 }
 
